76da1129769a82c03048d2fe63403aa04b337869
KedricNees01 <kedricnees@gmail.com>
2023-09-24 16:27:20 -0400

    Remove unnecessary files

--- app.py
+++ app.py
@@ -5,23 +5,25 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return "<p><a href='/'>Home</a> | <a href='/day'>Day</a> | <a href='/month'>Month</a> | <a href='/year'>Year</a></p>"
 
 @app.route("/day")
 def day():
     date = datetime.date.today()
     day = date.day
-    return render_template('day.html', day = day)
+    return f"<p>The current day is: { day }</p> <br> <p><a href = '/'>Home</a>"
 
 @app.route("/month")
 def month():
     date = datetime.date.today()
     month = date.strftime('%B')  
-    return render_template('month.html' , month = month)
+    return f"<p>The current month is: { month }</p> <br> <p><a href = '/'>Home</a>"
 
 @app.route("/year")
 def year():
-    return render_template('year.html')
+    date = datetime.date.today()
+    year = date.year
+    return f"<p>The current year is: { year }</p> <br> <p><a href = '/'>Home</a>"
 
 if __name__ == '__main__':
     app.run(debug=True)
