f60951f4adfc37a64ae3b2d57201c01891cc2237
KedricNees01 <kedricnees@gmail.com>
2023-09-24 15:56:11 -0400

    Add html templates

--- app.py
+++ app.py
@@ -1,8 +1,29 @@
-from flask import Flask
-
+from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template('index.html')
+
+@app.route("/day")
+def day():
+    date = datetime.date.today()
+    day = date.day
+    return render_template('day.html', day = day)
+
+@app.route("/month")
+def month():
+    date = datetime.date.today()
+    month = date.strftime('%B')  
+    return render_template('month.html' , month = month)
+
+@app.route("/year")
+def year():
+    return render_template('year.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
