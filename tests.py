dbf12a3fa8672c5b4e4d6abf45737c8d18795111
KedricNees01 <kedricnees@gmail.com>
2023-09-26 10:34:23 -0400

    Make tests dynamic

--- tests.py
+++ tests.py
@@ -1,13 +1,18 @@
 from app import app
+import datetime
 
 if __name__ == '__main__':
     with app.test_client() as client:
+        date = datetime.date.today()
+        day = str(date.day).encode('utf-8')
+        month = (date.strftime('%B')).encode('utf-8')
+        year = str(date.year).encode('utf-8')
         #Test the day route
         response = client.get('/day')
-        assert b'The current day is' in response.data
+        assert day in response.data
         #Test the month route
         response = client.get('/month')
-        assert b'The current month is' in response.data
+        assert month in response.data
         #Test the year route
         response = client.get('/year')
-        assert b'The current year is' in response.data
+        assert year in response.data
